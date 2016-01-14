dedca769d224479e7f956f5c6ab8a81c20bc52e2
deeNbi <lakeni@naver.com>
2016-01-14 15:25:39 +0900

    added output "what is DVDS?"

--- Hello.java
+++ Hello.java
@@ -2,5 +2,9 @@ public class Hello{
 	public static void main(String[] args)
 	{
 	System.out.println("Hello java!");
+	System.out.println("what is git?");
+	System.out.println("what's DVC?");
 	}
+
+
 }
