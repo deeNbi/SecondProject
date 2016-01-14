e82eba6bffcccdaf41b53498c332eec04ebec5d9
deeNbi <lakeni@naver.com>
2016-01-14 15:30:33 +0900

    added DVCS

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello{
 	public static void main(String[] args)
 	{
 	System.out.println("Hello java!");
+	System.out.println("what is Git?");
+	System.out.println("Distributed Version Control System");
 	}
 }
